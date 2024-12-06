c50cc0c4663afadb729b4839c808076a3e1b2d1f
Alben Bustamante <albenbustamante@gmail.com>
2024-12-06 12:27:23 -0500

    update: design avatars section

--- lib/pages/project_page.dart
+++ lib/pages/project_page.dart
@@ -15,7 +15,7 @@ class _ProjectPageState extends State<ProjectPage> {
     return Scaffold(
         backgroundColor: Colors.grey.shade100,
         body: Column(
-          children: [_header()],
+          children: [_header(), _avatars(), const Divider()],
         ));
   }
 
@@ -54,6 +54,38 @@ class _ProjectPageState extends State<ProjectPage> {
           color: Colors.grey[300], borderRadius: BorderRadius.circular(15.0)),
       child: IconButton(
           icon: Icon(icon, size: 28.0, color: Colors.blue), onPressed: () {}),
+    );
+  }
+
+  Container _avatars() {
+    return Container(
+        padding: const EdgeInsets.only(left: 25.0, top: 25.0),
+        height: 130.0,
+        child: ListView(scrollDirection: Axis.horizontal, children: [
+          _avatar('Mia', 'mia'),
+          _avatar('Adam', 'adam'),
+          _avatar('Jess', 'jess'),
+          _avatar('Mike', 'mike'),
+          _avatar('Brandon', 'brandon'),
+          _avatar('Alie', 'alie')
+        ]));
+  }
+
+  Padding _avatar(String name, String fileName) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 10.0),
+      child: Column(
+        children: [
+          CircleAvatar(
+              radius: 30.0,
+              backgroundImage:
+                  Image.asset('assets/images/$fileName.jpeg').image),
+          const SizedBox(height: 8.0),
+          Text(name,
+              style:
+                  const TextStyle(fontSize: 15.0, fontWeight: FontWeight.w500))
+        ],
+      ),
     );
   }
 }
